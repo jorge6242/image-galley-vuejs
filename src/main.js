050ac7342379fde3acc4e9274d6c999760e2f12a
Jorge Gomez <jorge6242@gmail.com>
2019-12-15 18:05:46 +0100

    add routes feature

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
 import Vue from 'vue'
+import Router from 'vue-router'
 import VueLazyload from 'vue-lazyload'
 
 import App from './App.vue'
 import Axios from './config/Axios'
+import { routes } from './routes'
 
 Vue.prototype.$http = Axios
+
 Vue.use(VueLazyload)
+
+Vue.use(Router)
+const router = new Router({ routes })
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
